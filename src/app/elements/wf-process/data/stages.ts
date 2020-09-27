52b18ee99eea9f85d8d195b76863621aa06f8bc7
lifeoflikhon <mehedi.hasansjs@gmail.com>
2020-09-27 19:31:34 +0600

    done with process

--- src/app/elements/wf-process/data/stages.ts
+++ src/app/elements/wf-process/data/stages.ts
@@ -4,6 +4,20 @@ export const stages: WfStageVm[] = [
   {
     id: 0,
     name: 'stage 1',
+    requiredReviewer: 1,
+    reviewers: [
+      {
+        id: 0,
+        name: 'md. abdul kader',
+        avatar: `https://randomuser.me/api/portraits/men/${Math.floor(Math.random() * 100)}.jpg`,
+        comment: 'I am approving this wired thing to buy.',
+        timestamp: '12/09/2020 10:08 AM',
+      }
+    ]
+  },
+  {
+    id: 0,
+    name: 'stage 1',
     requiredReviewer: 2,
     reviewers: [
       {
